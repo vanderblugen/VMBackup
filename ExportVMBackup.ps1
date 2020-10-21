1a726dde50ce02a37164c83563d79feed5a50414
vanderblugen <33649889+vanderblugen@users.noreply.github.com>
2020-10-21 10:04:44 -0500

    Update ExportVMBackup.ps1

--- ExportVMBackup.ps1
+++ ExportVMBackup.ps1
@@ -1,30 +1,20 @@
-# This script creates a backup of HyperV VM and exports it to a specified folder
-# The number of backups is limited
+# Removes old backups according to NumberOfBackupsToKeep and 
+# Exported the HyperVName and puts them into FolderName
 # It also writes to the EventViewer
 
-$NumberOfBackupsToKeep = 8                  # number of backups to keep
-$VMName = "VMName"                          # name of the virtual machine
-$FolderName = "E:\WhereverYouWant"          # where the exported/backup VM goes
+$NumberOfBackupsToKeep = 8
+$VMName = "Chaos"
+$FolderName = "E:\Exported Virtual Machines"
 
 New-EventLog –LogName Application –Source “Weekly $VMName HyperV Backup“
-# After running this script one time, this line may produce an error, this is normal
+# After running one time, this line may produce an error, this is normal
 
-While((Get-ChildItem -Path $FolderName | Measure-Object).Count -ge $NumberOfBackupsToKeep)
-# runs a loop as long as the number of folders in $FolderName is greater than or equal to $NumberOfBackupsToKeep
+While((Get-ChildItem -Path $FolderName | Measure-Object).Count -ge $NumberOfBackupsToKeep) 
 {
-
     $Item = Get-ChildItem -Path $FolderName | Sort CreationTime | select -First 1
-    # counts entries in the folder and outputs the oldest name by creation date
-    
     Write-EventLog -LogName "Application" -Source "Weekly $VMName HyperV Backup" -EventID 3001 -EntryType Information -Message "Removing HyperV Exported Backup for $VMName at Folder $($Item.FullName)"
-    # creates an entry in Event Viewer stating that the Exported VM folder was removed
-    
     rmdir $($Item.FullName) -r
-    # removes the oldest folder
 }
 
-Export-VM $VMName -Path "$FolderName\$VMName $(Get-Date -Format "yyyyMMdd hhmm")"
-# export the VM to the specified folder
-
-Write-EventLog -LogName "Application" -Source "Weekly $VMName HyperV Backup" -EventID 3001 -EntryType Information -Message "Exported HyperV $VMName to $FolderName\$VMName $(Get-Date -Format "yyyyMMdd hhmm")"
-# creates an entry in Event Viewer stating that the VM was backed up to a specified folder
+Export-VM $VMName -Path "$FolderName\$VMName $(Get-Date -Format "yyyyMMdd HHmm")"
+Write-EventLog -LogName "Application" -Source "Weekly $VMName HyperV Backup" -EventID 3001 -EntryType Information -Message "Exported HyperV $VMName to $FolderName\$VMName $(Get-Date -Format "yyyyMMdd HHmm")"
